b77c0fee8f0ff17391dabe8000f08463122754c8
Omar Ebrahim Mohamed Khalil <200012464@must.edu.eg>
2024-08-05 03:35:56 +0300

    First Question

--- Program.cs
+++ Program.cs
@@ -1,10 +1,37 @@
-﻿namespace LINQ
+﻿using static LINQ.ListGenerator;
+namespace LINQ
+
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q1
+            #region Point 1
+            //var outStock = ProductsList.Where(P => P.UnitsInStock == 0);
+            //foreach(var o in outStock)
+            //{
+            //    Console.WriteLine(o);
+            //}
+            #endregion
+            #region Point 2
+            //var inStock = ProductsList.Where(p => p.UnitPrice > 3 );
+            //foreach (var product in inStock)
+            //{
+            //    Console.WriteLine(product);
+            //}
+            #endregion
+            #region Point 3
+            //string[] Arr = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+            //for (int i = 0; i < Arr.Length; i++)
+            //{
+            //    if (Arr[i].Length < i){
+            //        Console.WriteLine(i);
+            //    }
+            //}
+            #endregion
+
+            #endregion
         }
     }
 }
